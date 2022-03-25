d33c9f0f3395263505c005c9dc9253a1956f7940
RitaM5 <sporshia108514orva@gmail.com>
2022-03-26 00:19:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import Shop from './Components/Shop/Shop'
+
+function App() {
+  return (
+    <div className="container">
+       <Shop></Shop>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='row'>
+           <h2 className="text-success text-center p-3">Bag World !!</h2>
+             <div className="col-lg-9 food-content">
+                 <h2>hwllooo</h2>
+                 <div className="row">
+                  
+                 </div>
+              </div>
+             <div className=" col-lg-3 cart-content bg-secondary mt-2">
+             <h2>cart</h2>
+          </div>
+        </div>
+    );
+};
+
+export default Shop;
